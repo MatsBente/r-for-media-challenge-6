d8004b7ce0557d9f83d60960b451e62dd556924d
Mats Bente <mats.bente@haw-hamburg.de>
2021-04-30 12:40:34 +0200

    final

--- challenge-6.R
+++ challenge-6.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(readr)
 library(stringr)
 library(purrr)
+library(ggrepel)
 
 # Stelle den mitgegebenen Plot nach. Alle Daten dafür habt Ihr ja vorliegen!
 
@@ -11,6 +12,22 @@ library(purrr)
 
 # load their Nebentätigkeiten
 # nebeneinkuenfte <- …
+
+#data
+
+bundestag_2019 <- readRDS("data/bundestag_2019.rds")
+abgeordnetenwatch <- read_csv("data/data-ROsix.csv") %>% 
+  janitor::clean_names() %>% 
+  mutate(
+    name = str_split(name, ", ") %>%
+      map_chr(~rev(.) %>% paste(collapse = " ")),
+    #name_ = map_chr(str_split(name, ", "), function(x) paste(rev(x), collapse = " ")) #andere Schreibweise
+    nebentaetigkeiten = nebentatigkeiten == "ja"
+  )%>% 
+  select(-nebentatigkeiten, -partei)
+
+Abgeordneten_full <- bundestag_2019 %>% 
+  left_join(abgeordnetenwatch)
 
 partei_farben <- list(
   "CDU" = "black",
@@ -23,6 +40,36 @@ partei_farben <- list(
   "fraktionslos" = "grey"
 )
 
+
+
+
+#plot
+options(scipen = 999) # e zu ganzen Zahlen
+
+Abgeordneten_full %>% 
+  ggplot(
+    aes(
+      x = lebensdaten,
+      y = land,
+      label = name
+    )
+  )+
+  geom_jitter(aes(
+    size = mindest_einkunfte_in_euro, 
+    color = fraktion
+  ))+
+  scale_color_manual(values = partei_farben)+
+  theme_minimal()+
+  labs(
+    x = "Jahrgang",
+    y = "Bundesland",
+    size = "Einkünfte",
+    color = "Partei"
+  )+
+  geom_label_repel(data = subset(Abgeordneten_full, mindest_einkunfte_in_euro > 469999)
+  )
+
+Abgeordneten_full
 # data <- …
 
 # ggplot(data) + …
